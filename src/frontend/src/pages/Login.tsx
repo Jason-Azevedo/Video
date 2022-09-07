a00cf4435381a1d84cc58bc20de078def6bce731
Jason Azevedo <jasonazevedo03@gmail.com>
2022-09-07 14:09:47 +0200

    Implemented form validation

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,31 +1,62 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import FormInput from "../components/inputs/FormInput";
+import { validateString } from "../validation/stringValidator";
 
 export default function Login() {
-  const emailRef = useRef(null);
-  const passRef = useRef(null);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({ email: "", password: "" });
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Validation
+    const emailResult = validateString({ notEmpty: true }, formData.email);
+    const passwordResult = validateString(
+      { notEmpty: true },
+      formData.password
+    );
+
+    if (emailResult.failed || passwordResult.failed) {
+      setErrors({
+        email: emailResult.message,
+        password: passwordResult.message,
+      });
+      return;
+    }
+
+    // clear errors
+    setErrors({ email: "", password: "" });
+
+    // Make the login request
+  };
 
   return (
-    <form className="form container--500" action="">
+    <form className="form container--500" onSubmit={onSubmit}>
       <h2 className="title--18 bold dim">Vidio</h2>
       <h1 className="title--24">Login</h1>
 
       {/* Email */}
-      {/* <FormInput
+      <FormInput
         label="Email"
-        error="Please fill in field"
-        onChange={console.log}
-      /> */}
+        value={formData.email}
+        error={errors.email}
+        onChange={(e) =>
+          setFormData((prev) => ({ ...prev, email: e.target.value }))
+        }
+      />
 
       {/* Password */}
-      {/* <FormInput
+      <FormInput
         password
         label="Password"
-        error="Please fill in field"
-        onChange={console.log}
-      /> */}
+        value={formData.password}
+        error={errors.password}
+        onChange={(e) =>
+          setFormData((prev) => ({ ...prev, password: e.target.value }))
+        }
+      />
 
       <p className="text--16 form-alt-text">
         Don't have an account?{" "}
--- src/frontend/src/pages/Signup.tsx
+++ src/frontend/src/pages/Signup.tsx
@@ -1,48 +1,113 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import FormInput from "../components/inputs/FormInput";
+import { validateString } from "../validation/stringValidator";
 
 export default function Signup() {
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
-  const passRef = useRef(null);
-  const confirmPassRef = useRef(null);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Validation
+    const usernameResult = validateString(
+      { notEmpty: true },
+      formData.username
+    );
+    const emailResult = validateString({ notEmpty: true }, formData.email);
+    const passwordResult = validateString(
+      { notEmpty: true },
+      formData.password
+    );
+    const confirmPasswordResult = validateString(
+      { notEmpty: true },
+      formData.confirmPassword
+    );
+
+    if (
+      usernameResult.failed ||
+      emailResult.failed ||
+      passwordResult.failed ||
+      confirmPasswordResult.failed
+    ) {
+      return setErrors({
+        username: usernameResult.message,
+        email: emailResult.message,
+        password: passwordResult.message,
+        confirmPassword: confirmPasswordResult.message,
+      });
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      return setErrors({
+        username: "",
+        email: "",
+        password: "Passwords don't match",
+        confirmPassword: "Passwords don't match",
+      });
+    }
+
+    // Submit the data
+  };
 
   return (
-    <form className="form container--500" action="">
+    <form className="form container--500" onSubmit={onSubmit}>
       <h2 className="title--18 bold dim">Vidio</h2>
       <h1 className="title--24">Signup</h1>
 
       {/* Username */}
-      {/* <FormInput
+      <FormInput
         label="Username"
-        error="Please fill in field"
-        onChange={console.log}
-      /> */}
+        value={formData.username}
+        error={errors.username}
+        onChange={(e) =>
+          setFormData((prev) => ({ ...prev, username: e.target.value }))
+        }
+      />
 
       {/* Email */}
-      {/* <FormInput
+      <FormInput
         label="Email"
-        error="Please fill in field"
-        onChange={console.log}
-      /> */}
+        value={formData.email}
+        error={errors.email}
+        onChange={(e) =>
+          setFormData((prev) => ({ ...prev, email: e.target.value }))
+        }
+      />
 
       {/* Password */}
-      {/* <FormInput
+      <FormInput
         password
         label="Password"
-        error="Please fill in field"
-        onChange={console.log}
-      /> */}
+        value={formData.password}
+        error={errors.password}
+        onChange={(e) =>
+          setFormData((prev) => ({ ...prev, password: e.target.value }))
+        }
+      />
 
       {/* Confirm Password */}
-      {/* <FormInput
+      <FormInput
         password
         label="Confirm Password"
-        error="Please fill in field"
-        onChange={console.log}
-      /> */}
+        value={formData.confirmPassword}
+        error={errors.confirmPassword}
+        onChange={(e) =>
+          setFormData((prev) => ({ ...prev, confirmPassword: e.target.value }))
+        }
+      />
 
       <p className="text--16 form-alt-text">
         Already have an account?{" "}
